f2af687deaf1b92ede416af2f0a58f7e5dff8b2f
max <maximiliangibes@gmail.com>
2023-01-21 07:25:28 -0700

    updates contact component with form, input fields and textarea.

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,22 +1,17 @@
 import React from "react";
 
 function Contact() {
-  // !! contact is going to be a form with name, email, and message
   return (
     <div>
-      <h1>Contact Information</h1>
-      <ul>
-        <li className="contact-list">Email: maximiliangibes@gmail.com</li>
-        <li className="contact-list">
-          <a href="https://github.com/Slimshady079" target="_blank">
-            <img
-              className="github-img"
-              src="../icons/icons8-github-48.png"
-              alt="pin Icon"
-            ></img>
-          </a>
-        </li>
-      </ul>
+      <form>
+        <h1>Contact Me below!</h1>
+        {/* name */}
+        <input type="text" placeholder="First & Last Name" name="name" />
+        {/* email */}
+        <input type="email" placeholder="Email" name="email" />
+        {/* message */}
+        <textarea type="text" placeholder="Enter your message" name="message"></textarea>
+      </form>
     </div>
   );
 }
